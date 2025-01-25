e317e2130c4b7dafecc122d1aa9ec1871404790c
Mikael Grön <skaramicke@gmail.com>
2025-01-25 13:10:51 +0100

    Fix indentation

--- src/main.go
+++ src/main.go
@@ -42,16 +42,16 @@ func main() {
 	fmt.Printf("Node %s hash: %s\n", nodeID, hash)
 
 	// Create single multicast socket
-    connections, err := discovery.CreateMulticastSockets(cfg)
-    if err != nil {
-        panic(err)
-    }
-    
-    for _, conn := range connections {
-        defer conn.Conn.Close()
-        go discovery.StartMulticastListener(cfg, &conn)
-        go discovery.StartBroadcast(cfg, hash, &conn)
-    }
+	connections, err := discovery.CreateMulticastSockets(cfg)
+	if err != nil {
+			panic(err)
+	}
+	
+	for _, conn := range connections {
+			defer conn.Conn.Close()
+			go discovery.StartMulticastListener(cfg, &conn)
+			go discovery.StartBroadcast(cfg, hash, &conn)
+	}
 	go api.StartHTTPServer()
 
 	select {}
